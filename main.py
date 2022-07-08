7727733620649b0aad4253a6ea1973607c993082
Brette Fitzgibbon <79230918+BretteConnolly@users.noreply.github.com>
2022-07-07 18:45:04 -0700

    Update main.py
    Refactored to make more concise. Used setheading method instead of conditionals with randint.

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 from turtle import Turtle, Screen
-from random import choice, randint
+from random import choice
 
 timmy = Turtle()
 timmy.pendown()
 timmy.width(10)
 
 color_list = ["turquoise", "cyan", "teal", "aquamarine", "light sky blue", "deep sky blue", "dodger blue", "cornflower blue", "blue", "medium blue"]
+directions = [0, 90, 180, 270]
 
-for i in range(100):
+for i in range(200):
   timmy.color(choice(color_list))
-  num = randint(1, 4)
-  if num == 1:
-    timmy.backward(50)
-  elif num == 2:
-    timmy.forward(50)
-  elif num == 3:
-    timmy.left(90)
-    timmy.forward(50)
-  else:
-    timmy.right(90)
-    timmy.forward(50)
-      
+  timmy.forward(30)
+  timmy.setheading(choice(directions))
+
+
 screen = Screen()
 screen.exitonclick()
